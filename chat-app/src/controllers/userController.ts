85654b88d701b09c67393a3cf4b37595f8440fae
Abhishek Gowda <abhi.gowda28@gmail.com>
2025-05-01 20:58:55 +0530

    feat: refactor user registration logic into controller and update user routes

--- chat-app/src/controllers/userController.ts
+++ chat-app/src/controllers/userController.ts
@@ -5,25 +5,55 @@ import { User } from '../models';
 import { validationResult } from 'express-validator';
 import { AppRoutes } from '../types/AppRoutes';
 
-interface LoginUser {
-    email: string;
-    password: string;
-};
+type RegisterRequest = Request<{}, {}, AppRoutes["/api/users/register"]["POST"]["body"]>;
+type RegisterResponse = Response<AppRoutes["/api/users/register"]["POST"]["response"]>;
 
-interface UserResponse {
-    message: string;
-    token: string;
-    user: {
-        _id: string;
-        username: string;
-        email: string;
+export const registerUser = async (req: RegisterRequest, res: RegisterResponse): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+    try {
+        const { username, email, password } = req.body;
+
+        const existingUser = await User.findOne({
+            $or: [
+                { username },
+                { email }
+            ]
+        });
+
+        if (existingUser) {
+            res.status(400).json({ message: "User already exists" });
+            return;
+        }
+
+        const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS) : 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            createdAt: newUser.createdAt,
+        });
+        return;
+    } catch (error) {
+        console.error("Error registering user", { error });
+        res.status(500).json({ message: "Server error" });
+        return;
     }
 }
 
-
 type LoginRequest = Request<{}, {}, AppRoutes["/api/users/login"]["POST"]["body"]>;
 type LoginResponse = Response<AppRoutes["/api/users/login"]["POST"]["response"]>;
-
 
 export const loginUser = async (req: LoginRequest, res: LoginResponse): Promise<void> => {
     const errors = validationResult(req);
--- chat-app/src/routes/userRoutes.ts
+++ chat-app/src/routes/userRoutes.ts
@@ -1,13 +1,7 @@
 import { Request, Response, Router } from "express";
-import bcrypt from "bcryptjs";
-import { User } from "../models"
-import { body, validationResult, } from "express-validator";
-import { AppRoutes } from "../types/AppRoutes";
+import { body } from "express-validator";
 import { protect } from "../middleware/auth";
-import { loginUser } from "../controllers/userController";
-
-type RegisterRequest = Request<{}, {}, AppRoutes["/api/users/register"]["POST"]["body"]>;
-type RegisterResponse = Response<AppRoutes["/api/users/register"]["POST"]["response"]>;
+import { loginUser, registerUser } from "../controllers/userController";
 
 const router = Router();
 
@@ -15,49 +9,7 @@ router.post("/register", [
     body("username").notEmpty().withMessage("Username is required"),
     body("email").isEmail().withMessage("Email is required"),
     body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
-], async (req: RegisterRequest, res: RegisterResponse): Promise<void> => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-        return;
-    }
-    try {
-        const { username, email, password } = req.body;
-
-        const existingUser = await User.findOne({
-            $or: [
-                { username },
-                { email }
-            ]
-        });
-
-        if (existingUser) {
-            res.status(400).json({ message: "User already exists" });
-            return;
-        }
-
-        const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS) : 10);
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword
-        });
-
-        await newUser.save();
-
-        res.status(201).json({
-            _id: newUser._id,
-            username: newUser.username,
-            email: newUser.email,
-            createdAt: newUser.createdAt,
-        });
-        return;
-    } catch (error) {
-        console.error("Error registering user", { error });
-        res.status(500).json({ message: "Server error" });
-        return;
-    }
-});
+], registerUser);
 
 router.post("/login", [
     body("email").isEmail().withMessage("Email is required"),
